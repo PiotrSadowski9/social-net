f1c2479be196951280e957899ec06da618182b6a
Piotr Sadowski <piotrsadowski008@gmail.com>
2021-03-22 17:49:00 +0100

    routing

--- src/app/layout/App.jsx
+++ src/app/layout/App.jsx
@@ -1,6 +1,10 @@
 import React, {useState} from 'react';
+import { Route } from 'react-router';
 import { Container } from 'semantic-ui-react';
 import EventDashboard from '../../features/events/eventDashboard/EventDashboard';
+import EventDetailedPage from '../../features/events/eventDetailed/EventDetailedPage';
+import EventForm from '../../features/events/eventForm/EventForm';
+import HomePage from '../../features/home/HomePage';
 import NavBar from '../../features/nav/NavBar';
 
 
@@ -23,7 +27,11 @@ function handleCreateFormOpen() {
     <>
       <NavBar setFormOpen={handleCreateFormOpen}/>
       <Container className={'main'}>
-        <EventDashboard formOpen={formOpen} setFormOpen={setFormOpen} selectEvent={handleSelectEvent} selectedEvent={selectedEvent}/>
+        {/* <EventDashboard formOpen={formOpen} setFormOpen={setFormOpen} selectEvent={handleSelectEvent} selectedEvent={selectedEvent}/> */}
+        <Route exact path='/' component={HomePage}/>
+        <Route exact path='/events' component={EventDashboard}/>
+        <Route path='/events/:id' component={EventDetailedPage}/>
+        <Route path='/createEvent' component={EventForm}/>
       </Container>
       
     </>
---
+++ src/features/home/HomePage.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import { Button, Container, Header, Icon, Image, Segment } from 'semantic-ui-react'
+
+export default function HomePage() {
+    return (
+        <Segment inverted textalign='center' vertical className='masthead'>
+            <Container>
+                <Header as='h1' inverted>
+                    <Image size='massive' src='/assets/logo.png' style={{marginBottom:12}}/>
+                    Re-vents
+                </Header>
+                <Button size='huge' inverted>
+                    Get started
+                    <Icon name='right arrow' inverted/>
+                </Button>
+            </Container>
+        </Segment>
+    )
+}
