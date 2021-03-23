9aa0aacfa19be2c31e2bdf4102f0780e2d193bbd
Piotr Sadowski <piotrsadowski008@gmail.com>
2021-03-23 23:02:30 +0100

    reduxreducers

---
+++ src/features/events/eventReducer.js
@@ -0,0 +1,29 @@
+import { sampleData } from "../../app/api/sampleData";
+import { DELETE_EVENT, UPDATE_EVENT, CREATE_EVENT } from "./eventConstants";
+
+
+const initialState = {
+    events:sampleData
+}
+
+export default function eventReducer(state = initialState, {type, payload}){
+    switch (type) {
+        case CREATE_EVENT:
+            return {
+                ...state,
+                events: [...state.events,payload] // dodaję nowy event do tablicy
+            };
+        case UPDATE_EVENT:
+            return {
+                ...state,
+                events: [...state.events.filter(evt => evt.id !== payload.id),payload] //filtruję tablicę eventów, szukam edytowanego eventu, usuwam i dodaję nowy
+            };
+        case DELETE_EVENT:
+            return {
+                ...state,
+                events:  [...state.events.filter(evt => evt.id !== payload)] // filtruję tablice i usuwam event
+            };
+        default:
+            return state;
+    }
+}
