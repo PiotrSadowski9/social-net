6721926d1c8acde938f85591afb411472845e798
Piotr Sadowski <piotrsadowski008@gmail.com>
2021-03-25 21:49:26 +0100

    LoginAuth

--- src/app/store/rootReducer.js
+++ src/app/store/rootReducer.js
@@ -2,12 +2,14 @@ import {combineReducers} from 'redux'
 import testReducer from '../../features/sandbox/testReducer'
 import eventReducer from '../../features/events/eventReducer'
 import modalReducer from '../common/modals/modalReducer'
+import authReducer from '../../features/auth/authReducer'
 
 
 const rootReducer = combineReducers({  //zamieniam obiekt z dwiema wartościami reducerów na jeden reducer
     test: testReducer,
     event: eventReducer,
-    modals:modalReducer
+    modals:modalReducer,
+    auth:authReducer
 })
 
 export default rootReducer
--- src/features/nav/NavBar.jsx
+++ src/features/nav/NavBar.jsx
@@ -1,17 +1,15 @@
-import React, { useState } from 'react'
-import { NavLink, useHistory } from 'react-router-dom'
+import React from 'react'
+import { useSelector } from 'react-redux'
+import { NavLink } from 'react-router-dom'
 import { Button, Container, Menu } from 'semantic-ui-react'
 import SignedInMenu from './SignedInMenu'
 import SignedOutMenu from './SignedOutMenu'
 
 export default function NavBar({setFormOpen}) {
-    const history = useHistory(); //daje dostep do hisotrii bez Routea
-    const [authenticated, setAuthenticated] = useState(false);
+    
+    const {authenticated} = useSelector(state => state.auth); //pobieram state ze stora
 
-    function handleSignOut(){
-        setAuthenticated(false);
-        history.push('/');
-    }
+    
     return (
         <Menu inverted fixed='top'>
             <Container>
@@ -25,7 +23,7 @@ export default function NavBar({setFormOpen}) {
                <Menu.Item as={NavLink} to='/createEvent'>
                    <Button positive inverted content="Create Event"/>
                </Menu.Item>}
-               {authenticated ? <SignedInMenu signOut={handleSignOut}/> : <SignedOutMenu setAuthenticated={setAuthenticated}/>}
+               {authenticated ? <SignedInMenu /> : <SignedOutMenu />}
             </Container>
         </Menu>
     )
