8f0d7cd8331ec4d8fe647d0d54090cdce2bcb07d
Piotr Sadowski <piotrsadowski008@gmail.com>
2021-03-28 20:21:58 +0200

    reduxThunk2

--- src/features/sandbox/Sandbox.jsx
+++ src/features/sandbox/Sandbox.jsx
@@ -8,13 +8,14 @@ export default function Sandbox() {
     const dispatch = useDispatch();
 
     const data = useSelector(state => state.test.data);
+    const {loading} = useSelector((state) => state.async)
 
     return (
         <>
            <h1>Testing 123</h1>
            <h3>The data is: {data}</h3> 
-           <Button onClick={() => dispatch(increment(20)) } content='Increment' color='green'/>
-           <Button onClick={() => dispatch(decrement(10)) } content='Decrement' color='red'/>
+           <Button loading={loading} onClick={() => dispatch(increment(20)) } content='Increment' color='green'/>
+           <Button loading={loading} onClick={() => dispatch(decrement(10)) } content='Decrement' color='red'/>
            <Button onClick={() => dispatch(openModal({modalType: 'TestModal',modalProps:{data}})) } content='Open Modal' color='teal'/>
         </> 
     )
