2d33e54ed8b542eda9ca3a11fbdba05c43140214
Piotr Sadowski <piotrsadowski008@gmail.com>
2021-03-28 19:59:34 +0200

    reduxThunk1

--- src/app/store/rootReducer.js
+++ src/app/store/rootReducer.js
@@ -3,13 +3,15 @@ import testReducer from '../../features/sandbox/testReducer'
 import eventReducer from '../../features/events/eventReducer'
 import modalReducer from '../common/modals/modalReducer'
 import authReducer from '../../features/auth/authReducer'
+import asyncReducer from '../async/asyncReducer'
 
 
 const rootReducer = combineReducers({  //zamieniam obiekt z dwiema wartościami reducerów na jeden reducer
     test: testReducer,
     event: eventReducer,
     modals:modalReducer,
-    auth:authReducer
+    auth:authReducer,
+    async: asyncReducer
 })
 
 export default rootReducer
