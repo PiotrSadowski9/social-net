b1871c8209f0272e27e887bbad331ca7601285f2
Piotr Sadowski <piotrsadowski008@gmail.com>
2021-03-30 21:48:35 +0200

    firebase listening

--- src/features/events/eventDashboard/EventDashboard.jsx
+++ src/features/events/eventDashboard/EventDashboard.jsx
@@ -1,6 +1,7 @@
-import React from 'react'
+import React, {useEffect} from 'react'
 import { useSelector } from 'react-redux'
 import { Grid } from 'semantic-ui-react'
+import { getEventsFromFirestore } from '../../../app/firestore/firestoreService';
 import EventFilters from './EventFilters';
 // import LoadingComponent from '../../../app/layout/LoadingComponent';
 import EventList from './EventList'
@@ -11,7 +12,12 @@ export default function EventDashboard() {
     const {events} = useSelector(state => state.event); //pobieram dane ze Stora
     const {loading} = useSelector(state => state.async);
 
-
+    useEffect(() => {
+        const unsubscribe = getEventsFromFirestore({
+            next: snapshot => console.log(snapshot.docs.map(docSnapshot => docSnapshot.data())), // po otrzymaniu danych z firestore
+            error: error => console.log(error)
+        })
+    })
    
     
     return (
