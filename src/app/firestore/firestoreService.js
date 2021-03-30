b6cb783468e24437ce56964c4ec766b262d543a9
Piotr Sadowski <piotrsadowski008@gmail.com>
2021-03-30 22:21:19 +0200

    firebase data

--- src/app/firestore/firestoreService.js
+++ src/app/firestore/firestoreService.js
@@ -2,6 +2,27 @@ import firebase from '../config/firebase';
 
 const db = firebase.firestore();
 
+export function dataFromSnapshot(snapshot) {
+    if (!snapshot.exists) return undefined; //property od razu otrzymana z snapshot
+    const data = snapshot.data();
+
+    for (const prop in data) { //sprawdzanie czy w prop danych jest data (w seknundach od 1970) i zamiana formatu na normalna datę
+        if (data.hasOwnProperty(prop)) {
+            if (data[prop] instanceof firebase.firestore.Timestamp) { // jeśli data jest Timestamp to mamy dostep do prop toDate();
+                data[prop] = data[prop].toDate();
+
+
+            }
+        }
+    }
+    
+    return {
+        ...data,
+        id:snapshot.id
+    }
+}
+
 export function getEventsFromFirestore(observer) {
     return db.collection('events').onSnapshot(observer)  //Sprawdzam czy działa
 }
+
--- src/features/events/eventDashboard/EventDashboard.jsx
+++ src/features/events/eventDashboard/EventDashboard.jsx
@@ -1,7 +1,8 @@
 import React, {useEffect} from 'react'
-import { useSelector } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
 import { Grid } from 'semantic-ui-react'
-import { getEventsFromFirestore } from '../../../app/firestore/firestoreService';
+import { dataFromSnapshot, getEventsFromFirestore } from '../../../app/firestore/firestoreService';
+import { listenToEvents } from '../eventActions';
 import EventFilters from './EventFilters';
 // import LoadingComponent from '../../../app/layout/LoadingComponent';
 import EventList from './EventList'
@@ -9,15 +10,17 @@ import EventListItemPlaceholder from './EventListItemPlaceholder';
 
 
 export default function EventDashboard() {
+    const dispatch = useDispatch();
     const {events} = useSelector(state => state.event); //pobieram dane ze Stora
     const {loading} = useSelector(state => state.async);
 
     useEffect(() => {
-        const unsubscribe = getEventsFromFirestore({
-            next: snapshot => console.log(snapshot.docs.map(docSnapshot => docSnapshot.data())), // po otrzymaniu danych z firestore
+        const unsubscribe = getEventsFromFirestore({ //Listening danych z firestora
+            next: snapshot => dispatch(listenToEvents(snapshot.docs.map(docSnapshot => dataFromSnapshot(docSnapshot)))), // otrzymywanie danych z firestore
             error: error => console.log(error)
         })
-    })
+        return unsubscribe
+    },[dispatch]) //uruchamiamy tylko jeśli dispatch jest na liście zmian. Powinien być tylko raz przy uruchamianiu
    
     
     return (
