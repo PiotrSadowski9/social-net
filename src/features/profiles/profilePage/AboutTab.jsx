8b1411d97503a58ac5373bb494a13dfad03bb193
Piotr Sadowski <piotrsadowski008@gmail.com>
2021-05-07 20:17:35 +0200

    profile form

--- src/features/profiles/profilePage/AboutTab.jsx
+++ src/features/profiles/profilePage/AboutTab.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react'
 import { Button, Grid, Header, Tab } from 'semantic-ui-react';
 import {format } from 'date-fns'
+import ProfileForm from './ProfileForm';
 
 
 export default function AboutTab({profile}) {
@@ -13,7 +14,7 @@ export default function AboutTab({profile}) {
                     <Button onClick={() => setEditMode(!editMode)} floated='right' basic content={editMode ? 'Cancel' : 'Edit'}/>
                 </Grid.Column>
                 <Grid.Column width={16}>
-                    {editMode ? <p>Profile form</p> : 
+                    {editMode ? <ProfileForm profile={profile}/> : 
                     <>
                         <div style={{marginBotton:10}}>
                             <strong>Member since: {format(profile.createdAt, 'dd MMM yyyy')}</strong>
