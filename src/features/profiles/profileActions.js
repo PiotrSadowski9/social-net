1b9f12a5987f2c3d1fadafbd83b75bc025f2d36b
Piotr Sadowski <piotrsadowski008@gmail.com>
2021-06-21 23:26:41 +0200

    displaying the image

--- src/features/profiles/profileActions.js
+++ src/features/profiles/profileActions.js
@@ -1,4 +1,4 @@
-import {LISTEN_TO_CURRENT_USER_PROFILE} from './profileConstants';
+import {LISTEN_TO_CURRENT_USER_PROFILE, LISTEN_TO_USER_PHOTOS} from './profileConstants';
 import {LISTEN_TO_SELECTED_USER_PROFILE} from './profileConstants'
 
 export function listenToCurrentUserProfile(profile) {
@@ -12,5 +12,13 @@ export function listenToSelectedUserProfile(profile) {
     return {
         type: LISTEN_TO_SELECTED_USER_PROFILE,
         payload: profile
+    }
+}
+
+export function listenToUserPhotos(photos) {
+    return {
+        type: LISTEN_TO_USER_PHOTOS,
+        payload: photos
+
     }
 }
