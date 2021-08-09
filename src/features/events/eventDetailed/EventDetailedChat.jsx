8c3fa461c3bb5b84e02781a5d684b992e39e8fef
Piotr Sadowski <piotrsadowski008@gmail.com>
2021-08-10 00:34:10 +0200

    chat comments

--- src/features/events/eventDetailed/EventDetailedChat.jsx
+++ src/features/events/eventDetailed/EventDetailedChat.jsx
@@ -1,9 +1,11 @@
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
+import { Link } from 'react-router-dom';
 import {Segment, Header, Comment} from 'semantic-ui-react';
 import { firebaseObjectToArray, getEventChatRef } from '../../../app/firestore/firebaseService';
 import { listenToEventChat } from '../eventActions';
 import EventDetailedChatForm from './EventDetailedChatForm';
+import { formatDistance } from 'date-fns';
 
 export default function EventDetailedChat({eventId}) {
 
@@ -31,69 +33,24 @@ export default function EventDetailedChat({eventId}) {
 
             <Segment attached>
                 <Comment.Group>
-                    <Comment>
-                        <Comment.Avatar src="/assets/user.png"/>
+                {comments.map(comment => (
+                    <Comment key={comment.id}>
+                        <Comment.Avatar src={comment.photoURL || '/assets/user.png'}/>
                         <Comment.Content>
-                            <Comment.Author as="a">Matt</Comment.Author>
+                            <Comment.Author as={Link} to={`/profile/${comment.uid}`}>{comment.displayName}</Comment.Author>
                             <Comment.Metadata>
-                                <div>Today at 5:42PM</div>
+                                <div>{formatDistance(comment.date, new Date())}</div>
                             </Comment.Metadata>
-                            <Comment.Text>How artistic!</Comment.Text>
+                            <Comment.Text>{comment.text}</Comment.Text>
                             <Comment.Actions>
                                 <Comment.Action>Reply</Comment.Action>
                             </Comment.Actions>
                         </Comment.Content>
-                    </Comment>
-
-                    <Comment>
-                        <Comment.Avatar src="/assets/user.png"/>
-                        <Comment.Content>
-                            <Comment.Author as="a">Elliot Fu</Comment.Author>
-                            <Comment.Metadata>
-                                <div>Yesterday at 12:30AM</div>
-                            </Comment.Metadata>
-                            <Comment.Text>
-                                <p>
-                                    This has been very useful for my research. Thanks as well!
-                                </p>
-                            </Comment.Text>
-                            <Comment.Actions>
-                                <Comment.Action>Reply</Comment.Action>
-                            </Comment.Actions>
-                        </Comment.Content>
-                        <Comment.Group>
-                            <Comment>
-                                <Comment.Avatar src="/assets/user.png"/>
-                                <Comment.Content>
-                                    <Comment.Author as="a">Jenny Hess</Comment.Author>
-                                    <Comment.Metadata>
-                                        <div>Just now</div>
-                                    </Comment.Metadata>
-                                    <Comment.Text>Elliot you are always so right :)</Comment.Text>
-                                    <Comment.Actions>
-                                        <Comment.Action>Reply</Comment.Action>
-                                    </Comment.Actions>
-                                </Comment.Content>
-                            </Comment>
-                        </Comment.Group>
-                    </Comment>
-
-                    <Comment>
-                        <Comment.Avatar src="/assets/user.png"/>
-                        <Comment.Content>
-                            <Comment.Author as="a">Joe Henderson</Comment.Author>
-                            <Comment.Metadata>
-                                <div>5 days ago</div>
-                            </Comment.Metadata>
-                            <Comment.Text>Dude, this is awesome. Thanks so much</Comment.Text>
-                            <Comment.Actions>
-                                <Comment.Action>Reply</Comment.Action>
-                            </Comment.Actions>
-                        </Comment.Content>
-                    </Comment>
-
-                   <EventDetailedChatForm eventId={eventId}/>
+                    </Comment>  
+                ))}
+                     
                 </Comment.Group>
+                <EventDetailedChatForm eventId={eventId}/>
             </Segment>
          </>
     )
