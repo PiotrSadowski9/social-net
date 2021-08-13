7ff5116d978c145d990d8b8e2ebe4861fdd8e235
Piotr Sadowski <piotrsadowski008@gmail.com>
2021-08-13 11:48:37 +0200

    comment replies

--- src/app/common/util/util.js
+++ src/app/common/util/util.js
@@ -5,3 +5,17 @@ export function delay(ms) {
 export function getFileExtension(filename) {
     return filename.slice((filename.lastIndexOf('.') -1 >>> 0) +2); //pobieranie formatu image
 }
+
+//drzewko komentarzy, przypisywanie odpowiedzi do komentarza
+
+export function createDataTree(dataset) { // jęsli parent elem ma 0 tzn że jest to oryginalny komentarz
+    let hashtable = Object.create(null);
+    dataset.forEach(a => hashtable[a.id] = {...a, childNodes: []});
+    let dataTree = [];
+    dataset.forEach(a => {
+        if (a.parentId) hashtable[a.parentId].childNodes.push(hashtable[a.id]);
+        else dataTree.push(hashtable[a.id]);
+    });
+    return dataTree
+}
+
