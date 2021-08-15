7197ad759566688914ab6418b5017e4720eb4491
Piotr Sadowski <piotrsadowski008@gmail.com>
2021-08-15 21:11:41 +0200

    followers data

--- src/features/profiles/profilePage/ProfileContent.jsx
+++ src/features/profiles/profilePage/ProfileContent.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Tab } from 'semantic-ui-react'
 import AboutTab from './AboutTab'
 import EventsTab from './EventsTab'
@@ -6,15 +6,21 @@ import FollowingTab from './FollowingTab'
 import PhotosTab from './PhotosTab'
 
 export default function ProfileContent({profile, isCurrentUser}) {
+    const [activeTab, setActiveTab] = useState(0);
     const panes = [
         {menuItem: 'About', render: ()=> <AboutTab profile={profile} isCurrentUser={isCurrentUser}/>},
         {menuItem: 'Photos', render: ()=> <PhotosTab profile={profile} isCurrentUser={isCurrentUser}/>},
         {menuItem: 'Events', render: ()=> <EventsTab profile={profile}/>},
-        {menuItem: 'Followers', render: ()=> <FollowingTab profile={profile}/>},
-        {menuItem: 'Following', render: ()=> <FollowingTab profile={profile}/>}
+        {menuItem: 'Followers', render: ()=> <FollowingTab profile={profile} activeTab={activeTab}/>},
+        {menuItem: 'Following', render: ()=> <FollowingTab profile={profile} activeTab={activeTab}/>}
     ]
     return (
-        <Tab menu={{fluid:true, vertical:true}} menuPosition='right' panes={panes} />
+        <Tab 
+            menu={{fluid:true, 
+            vertical:true}} 
+            menuPosition='right' 
+            panes={panes}
+            onTabChange={(e, data) => setActiveTab(data.activeIndex)} />
             
         
     )
