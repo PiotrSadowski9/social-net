618d77b525e045b1238fa34346d831312e34f628
Piotr Sadowski <piotrsadowski008@gmail.com>
2021-08-15 22:26:56 +0200

    followers status

--- src/features/profiles/profilePage/ProfileHeader.jsx
+++ src/features/profiles/profilePage/ProfileHeader.jsx
@@ -1,16 +1,39 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
+import { useDispatch, useSelector } from 'react-redux';
 import { toast } from 'react-toastify';
 import { Button, Divider, Grid, Header, Item, Reveal, Segment, Statistic } from 'semantic-ui-react'
-import { followUser, unfollowUser } from '../../../app/firestore/firestoreService';
+import { followUser, getFollowingDoc, unfollowUser } from '../../../app/firestore/firestoreService';
+import { setFollowUser, setUnfollowUser } from '../profileActions';
+import ProfileCard from './ProfileCard';
 
 export default function ProfileHeader({profile, isCurrentUser}) {
+    const dispatch = useDispatch();
     const [loading, setLoading] = useState(false);
+    const {followingUser} = useSelector(state => state.profile);
+
+    useEffect(() => {
+        if (isCurrentUser) return;
+        setLoading(true);
+        async function fetchFollowingDoc() {
+            try {
+                const followingDoc = await getFollowingDoc(profile.id);
+                if (followingDoc && followingDoc.exists) {
+                    dispatch(setFollowUser())
+                }
+            } catch (error) {
+                toast.error(error.message);
+            }
+        }
+        fetchFollowingDoc().then(() => setLoading(false))
+    }, [dispatch, profile.id, isCurrentUser])
+
 
 
     async function handleFollowUser() {
         setLoading(true);
         try {
-            await followUser(profile)
+            await followUser(profile);
+            dispatch(setFollowUser())
         } catch (error) {
             toast.error(error.message)
         } finally {
@@ -21,7 +44,8 @@ export default function ProfileHeader({profile, isCurrentUser}) {
     async function handleUnfollowUser() {
         setLoading(true);
         try {
-            await unfollowUser(profile)
+            await unfollowUser(profile);
+            dispatch(setUnfollowUser())
         } catch (error) {
             toast.error(error.message)
         } finally {
@@ -44,22 +68,28 @@ export default function ProfileHeader({profile, isCurrentUser}) {
                 </Grid.Column>
                 <Grid.Column width={4}>
                     <Statistic.Group>
-                        <Statistic label='Followers' value={10}/>
-                        <Statistic label= 'Following' value={5}/>
+                        <Statistic label='Followers' value={profile.followerCount || 0}/>
+                        <Statistic label= 'Following' value={profile.followingCount || 0}/>
                     </Statistic.Group>
                     {!isCurrentUser &&
                     <>
                         <Divider/>
                         <Reveal animated='move'>
                             <Reveal.Content visible style={{width:'100%'}}>
-                                <Button fluid color='teal' content='Following'/>
+                                <Button fluid color='teal' content={followingUser ? 'Following' : 'Not following'}/>
                             </Reveal.Content>
                             <Reveal.Content hidden style={{width:'100%'}}>
-                                <Button onClick={handleFollowUser} loading={loading} basic fluid color='green' content='Follow'/>
+                                <Button 
+                                    onClick={followingUser ? () => handleUnfollowUser() : () => handleFollowUser()} 
+                                    loading={loading} 
+                                    basic
+                                    fluid
+                                    color={followingUser ? 'red' : 'green'} 
+                                    content={followingUser ? 'Unfollow' : 'Follow'}/>
                             </Reveal.Content>
                             
                         </Reveal>
-                        <Button onClick={handleUnfollowUser} loading={loading} basic fluid color='red' content='Unfollow'/>
+                        
                     </>}
                     
                 </Grid.Column>
