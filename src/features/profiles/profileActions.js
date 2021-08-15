5cd39ac84c547643e6145e47dea2296e5f161257
Piotr Sadowski <piotrsadowski008@gmail.com>
2021-08-15 20:39:35 +0200

    following actions

--- src/features/profiles/profileActions.js
+++ src/features/profiles/profileActions.js
@@ -1,4 +1,4 @@
-import {LISTEN_TO_CURRENT_USER_PROFILE, LISTEN_TO_USER_PHOTOS, LISTEN_TO_USER_EVENTS} from './profileConstants';
+import {LISTEN_TO_CURRENT_USER_PROFILE, LISTEN_TO_USER_PHOTOS, LISTEN_TO_USER_EVENTS, LISTEN_TO_FOLLOWERS, LISTEN_TO_FOLLOWINGS} from './profileConstants';
 import {LISTEN_TO_SELECTED_USER_PROFILE} from './profileConstants'
 
 export function listenToCurrentUserProfile(profile) {
@@ -27,5 +27,19 @@ export function listenToUserEvents(events) {
     return {
         type: LISTEN_TO_USER_EVENTS,
         payload: events
+    }
+}
+
+export function listenToFollowers(followers) {
+    return {
+        type: LISTEN_TO_FOLLOWERS,
+        payload: followers
+    }
+}
+
+export function listenToFollowings(followings) {
+    return {
+        type: LISTEN_TO_FOLLOWINGS,
+        payload: followings
     }
 }
