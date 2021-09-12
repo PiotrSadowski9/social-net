b8529e0804358d076cad4f2a93a52efd8e92de72
Piotr Sadowski <piotrsadowski008@gmail.com>
2021-09-12 23:45:34 +0200

    feeds

--- functions/index.js
+++ functions/index.js
@@ -1,3 +1,4 @@
+/* eslint-disable max-len */
 
 const admin = require("firebase-admin");
 const functions = require("firebase-functions");
@@ -48,3 +49,52 @@ exports.removeFollowing = functions.firestore
         return console.log(error);
       }
     });
+
+exports.eventUpdated = functions.firestore
+    .document("events/{eventId}")
+    .onUpdate(async (snapshot, context) => {
+      const before = snapshot.before.data();
+      const after = snapshot.after.data();
+      if (before.attemdees.length < after.attendees.length) {
+        // eslint-disable-next-line max-len
+        const attendeeJoined = after.attendees.filter((item1) => !before.attendees.some((item2) =>
+          item2.id === item1.id))[0];
+        console.log({attendeeJoined});
+        try {
+          const followerDocs = await db.collection("following").doc(attendeeJoined.id)
+              .collection("userFollowers").get();
+          followerDocs.forEach((doc) => {
+            admin.database().ref(`/posts/${doc.id}`).push(newPost)(attendeeJoined, "joined-event", context.params.eventId);
+          });
+        } catch (error) {
+          return console.log(error);
+        }
+      }
+      if (before.attemdees.length > after.attendees.length) {
+        // eslint-disable-next-line max-len
+        const attendeeLeft = before.attendees.filter((item1) => !after.attendees.some((item2) =>
+          item2.id === item1.id))[0];
+        console.log({attendeeLeft});
+        try {
+          const followerDocs = await db.collection("following").doc(attendeeLeft.id)
+              .collection("userFollowers").get();
+          followerDocs.forEach((doc) => {
+            admin.database().ref(`/posts/${doc.id}`).push(newPost)(attendeeLeft, "left-event", context.params.eventId);
+          });
+        } catch (error) {
+          return console.log(error);
+        }
+      }
+    });
+
+// eslint-disable-next-line require-jsdoc
+function newPost(user, code, eventId) {
+  return {
+    photoURL: user.photoURL || "/assets/user.png",
+    date: admin.database.ServerValue.TIMESTAMP,
+    code,
+    displayName: user.displayName,
+    eventId,
+    userUid: user.id,
+  };
+}
--- src/features/events/eventDashboard/EventListAttendee.jsx
+++ src/features/events/eventDashboard/EventListAttendee.jsx
@@ -5,7 +5,7 @@ import { Image, List } from 'semantic-ui-react'
 export default function EventListAttendee({attendee}) {
     return (
         <List.Item as={Link} to={`/profile/${attendee.id}`}>
-            <Image size='mini' circular src={attendee.photoURL}/>
+            <Image size='mini' circular src={attendee.photoURL || '/assets/user.png'}/>
         </List.Item>
     )
 }
